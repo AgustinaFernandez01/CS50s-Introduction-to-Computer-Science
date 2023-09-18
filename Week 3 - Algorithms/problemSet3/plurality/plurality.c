2d24859694a2a686974450ef2915f375ccca7b20
AgustinaFernandez01 <agustinafer29@gmail.com>
2023-09-18 18:55:02 -0300

    third commit

---
+++ Week 3 - Algorithms/problemSet3/plurality/plurality.c
@@ -0,0 +1,66 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+} candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // ... (rest of the main function remains the same)
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true; // Valid vote
+        }
+    }
+    return false; // Invalid vote
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    int max_votes = 0;
+
+    // Find the maximum number of votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > max_votes)
+        {
+            max_votes = candidates[i].votes;
+        }
+    }
+
+    // Print the names of candidates with the maximum votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+}
