bdf42a7a57c03311c4b66f2defba6294d8902f86
AgustinaFernandez01 <agustinafer29@gmail.com>
2023-09-20 15:34:12 -0300

    commit

---
+++ Week 7 - SQL/problemSet7/movies/12.sql
@@ -0,0 +1,9 @@
+SELECT m.title
+FROM movies m
+JOIN stars s1 ON m.id = s1.movie_id
+JOIN people p1 ON s1.person_id = p1.id
+JOIN stars s2 ON m.id = s2.movie_id
+JOIN people p2 ON s2.person_id = p2.id
+WHERE (p1.name = 'Bradley Cooper' OR p1.name = 'Jennifer Lawrence')
+  AND (p2.name = 'Bradley Cooper' OR p2.name = 'Jennifer Lawrence')
+  AND p1.name != p2.name;
